3850c8efabcdba90be44a30c70b49b25be5eaebf
HelloZXB <Hello16369@163.com>
2021-01-20 15:58:25 +0800

    文章三图

--- src/components/Layout/CenterComponent/CenterContent.vue
+++ src/components/Layout/CenterComponent/CenterContent.vue
@@ -65,6 +65,41 @@
           </div>
         </div>
       </div>
+
+      <!-- 三图 -->
+      <div class="article">
+        <div class="article-img3">
+          <img src="https://www.xcnte.com/usr/uploads/2020/12/2247044869.png" alt="">
+          <img src="https://www.xcnte.com/usr/uploads/2020/12/2247044869.png" alt="">
+          <img src="https://www.xcnte.com/usr/uploads/2020/12/2247044869.png" alt="">
+        </div>
+        <div class="article-pa">
+          <h2>吉他拨弦方法</h2>
+          <p>持琴方式翘腿式两腿分开式两个接触点：右腿部右手拨弦方法右手拨弦，左手按弦最粗是6弦，最细是1弦右手小指一般用不到，4根手指需要弹6弦6 5 4弦 用大拇指...</p>
+          <div class="features">
+          <span>
+            <Icon type="ios-person"/>
+            HELLOZXB
+          </span>
+            <span>
+            <Icon type="ios-time-outline"/>
+            2021.01.20
+          </span>
+            <span>
+            <Icon type="md-eye"/>
+            3234 浏览
+          </span>
+            <span>
+            <Icon type="ios-thumbs-up-outline"/>
+            1285 点赞
+          </span>
+            <span>
+            <Icon type="ios-chatbubbles-outline"/>
+            918 评论
+          </span>
+          </div>
+        </div>
+      </div>
     </Content>
   </div>
 </template>
@@ -85,6 +120,7 @@
       margin-top: 30px;
       border-radius: 6px;
       box-shadow: 1px 1px 5px 5px rgb(169 169 169 / 35%);
+      overflow: hidden;
 
       .article-pa {
         padding: 0 10px;
@@ -94,6 +130,18 @@
         img {
           width: 100%;
           height: 100%;
+        }
+      }
+
+      .article-img3 {
+        img {
+          width: 32.333%;
+          height: 33.333%;
+          margin-left: 13px;
+        }
+
+        img:first-child {
+          margin-left: 0;
         }
       }
 
