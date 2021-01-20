675de4992b7d59185dee6e036f17f8f23fb134ec
HelloZXB <Hello16369@163.com>
2021-01-20 19:33:02 +0800

    初始化

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    ['import', {
-      libraryName: 'muse-ui',
-      libraryDirectory: 'lib',
-      camel2DashComponentName: false
-    }, 'muse-ui']
   ]
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-const Layout = () => import('@/views/Layout')
-const Home = () => import('@/views/Layout/home/Home')
-
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    component: Layout,
-    children: [
-      {
-        path: '/',
-        component: Home,
-        meta: { isKeepAlive: true } // 元信息, 额外的参数
-      }
-    ]
-  }
-]
+const routes = []
 
 const router = new VueRouter({
   routes
