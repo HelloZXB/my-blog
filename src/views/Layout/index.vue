ccc2cc48f22dcba3650d2419f3036c0464a5cf97
HelloZXB <Hello16369@163.com>
2021-01-21 13:04:35 +0800

    框架搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    主页
+  <!-- 主页 -->
+  <div class="layout">
+    <el-container class="el-shadow">
+      <!-- 头部 -->
+      <el-header>
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="grid-content bg-purple"></div>
+          </el-col>
+          <el-col :span="18">
+            <div class="grid-content bg-purple"></div>
+          </el-col>
+        </el-row>
+      </el-header>
+
+      <el-container>
+
+        <!-- 侧边栏 -->
+        <el-aside>
+        </el-aside>
+        <el-container>
+
+          <!-- 内容区域 -->
+          <el-main>Main</el-main>
+
+          <!-- 尾部 -->
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -10,6 +38,6 @@
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  @import "src/styles/Layout/Layout";
 </style>
